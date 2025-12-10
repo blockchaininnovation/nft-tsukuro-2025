4aaee8eea3a60c42bb0209a5449909327e7b5e84
tsukasa chibana <t-chibana@se-ec.co.jp>
2025-12-10 23:40:47 +0900

    fix lint error

--- front/src/app/token/page.tsx
+++ front/src/app/token/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 const DEFAULT_TEAM = "c";
 
