f1ab143c35fb32324f193431691a0232cca48aea
Z9RTM <moppi624@yahoo.co.jp>
2025-12-16 11:33:07 +0000

    fix: improve formatting of ethereum request and removeListener methods for better readability

--- front/src/types/window.d.ts
+++ front/src/types/window.d.ts
@@ -1,9 +1,15 @@
 declare global {
   interface Window {
     ethereum?: {
-      request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+      request: (args: {
+        method: string;
+        params?: unknown[];
+      }) => Promise<unknown>;
       on: (event: string, handler: (...args: unknown[]) => void) => void;
-      removeListener: (event: string, handler: (...args: unknown[]) => void) => void;
+      removeListener: (
+        event: string,
+        handler: (...args: unknown[]) => void,
+      ) => void;
     };
   }
 }
